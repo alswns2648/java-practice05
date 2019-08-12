a0c539bba06660f1a0714c29ef6e1a80be06fd7e
BIT <BIT@DESKTOP-7P1ENO8>
2019-08-12 21:32:43 +0900

    5번까지 완료

--- src/prob3/Prob2.java
+++ src/prob3/Prob2.java
@@ -2,6 +2,7 @@ package prob3;
 
 public class Prob2 {
 	public static void main(String[] args) {
+		
 		Bird bird01 = new Duck();
 		bird01.setName( "꽥꽥이" );
 		bird01.fly();
@@ -9,7 +10,7 @@ public class Prob2 {
 		System.out.println( bird01 );
 		
 		Bird bird02 = new Sparrow();
-		bird02.setName( "짹짹이" );
+		bird02.setName( "짹짹" );
 		bird02.fly();
 		bird02.sing();
 		System.out.println( bird02 );
--- src/prob5/MyStack.java
+++ src/prob5/MyStack.java
@@ -1,8 +1,42 @@
 package prob5;
 
-public class MyStack<T> {
-	private int top;
-	private String[] buffer;
+//stack 만드록 pop동작
+public class MyStack {
 
-	
+	private int top = -1; 
+	private String[] buffer; 
+
+	public MyStack(int i) {
+		buffer = new String[i];		
+	}
+
+	public void push(String s){
+		if(top+1 < buffer.length) {
+			buffer[++top] = s;
+		}else {
+			String[] mybuffer = new String[buffer.length*2];
+			for(int i = 0; i < buffer.length; i++) {
+				mybuffer[i]=buffer[i];
+			}
+			mybuffer[++top]=s; // top : 값이 입력되는 장소 
+			// top : 배열 몇개가 차있는지 
+			buffer = mybuffer; 
+		}
+	}
+
+	public String pop() throws MyStackException{
+		if(isEmpty()) {
+			throw new MyStackException("stack is empty");
+		}else {
+			return buffer[top--];
+		}
+	}
+
+	public boolean isEmpty() {
+		if(top == -1){
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
